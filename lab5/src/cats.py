0783d6d430322926c1736b250f34e3025e3ac00b
aya47 <84458459+aya47@users.noreply.github.com>
2025-02-24 23:36:01 -0600

    Initial implementation of Calico

--- lab5/src/cats.py
+++ lab5/src/cats.py
@@ -12,19 +12,27 @@ class Tabby(Cat):
     """
     def __init__(self, initial_rest_time : int):
         super().__init__('tabby', 4, 4, initial_rest_time)
-
+  
 class Calico(Cat):
     """
     A particularly clever cat
     They become less distractable after each rest
     Concretely, a Calico's _starting_rest_time increases by one 
       each time the Calico's attention is reduced to zero
+
+    Also, from pdf instructions: increase _starting_attention by 1
     """
     # TODO: Part 2: implement this class!
     # You may find adding new methods to be helpful
     def __init__(self, initial_rest_time : int):
-        super().__init__('calico', 1, 1, 0)
-        assert False, "Calico unimplemented"
+        super().__init__('calico', 1, 1, initial_rest_time)
+        #assert False, "Calico unimplemented"
+    def end_round(self):
+        # when cat rests, add 1 to its _starting_attention
+        if not Cat.Calico.is_on_board():
+            self._starting_attention += 1
+
+
 
 class Kitten(Cat):
     """
